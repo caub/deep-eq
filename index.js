a462eab732c8685430897a58e6ade646996b0aaf
caub <cyril.auburtin@gmail.com>
2017-11-18 11:14:50 +0100

    better tests

--- index.js
+++ index.js
@@ -14,9 +14,22 @@ class AssertionError extends Error {
 	}
 }
 
+const leftDiff = (keyStr, v2) => `${chalk.bold.red('\n → ' + keyStr)}: ${chalk.magentaBright(format(v2))}`;
+const rightDiff = (keyStr, v1) => `${chalk.bold.red('\n ← ' + keyStr)}: ${chalk.magenta(format(v1))}`;
+const bothDiff = (keyStr, v1, v2) => `${chalk.bold.red('\n ↔ ' + keyStr)}: ${chalk.magenta(format(v1))} !== ${chalk.magentaBright(format(v2))}`;
+
+
 const deepEqual = (o1, o2) => {
 	const diffs = [];
 	const deepEq = (o1, o2, path = []) => {
+		const o1IsObj = typeof o1 === 'object' && o1 !== null;
+		const o2IsObj = typeof o2 === 'object' && o2 !== null;
+		if (!o1IsObj || !o2IsObj) {
+			if (o1 !== o2) {
+				diffs.push(bothDiff(path.join('.'), o1, o2));
+			}
+			return;
+		}
 		const keys = new Set(Object.keys(o1).concat(Object.keys(o2)));
 		keys.forEach(key => {
 			const v1 = o1[key], v2 = o2[key];
@@ -24,13 +37,13 @@ const deepEqual = (o1, o2) => {
 			if (v1 === undefined && v2 === undefined) {
 				// ignore 
 			} else if (v1 === undefined) {
-				diffs.push(`${chalk.bold.red('\n → ' + keyStr)}: ${chalk.magentaBright(format(v2))}`);
+				diffs.push(leftDiff(keyStr, v2));
 			} else if (v2 === undefined) {
-				diffs.push(`${chalk.bold.red('\n ← ' + keyStr)}: ${chalk.magenta(format(v1))}`);
+				diffs.push(rightDiff(keyStr, v1));
 			} else if (v1 && typeof v1 === 'object' && v2 && typeof v2 === 'object') {
 				deepEq(v1, v2, path.concat(key));
 			} else if (v1 !== v2) {
-				diffs.push(`${chalk.bold.red('\n ↔ ' + keyStr)}: ${chalk.magenta(format(v1))} !== ${chalk.magentaBright(format(v2))}`);
+				diffs.push(bothDiff(keyStr, v1, v2));
 			}
 		});
 	};
--- test.js
+++ test.js
@@ -1,4 +1,29 @@
+const strip = require('strip-ansi');
 const eq = require('.');
+
+try {
+	eq(null, undefined);
+} catch (e){
+	console.assert(strip(e.message).includes('null !== undefined'));
+}
+
+try {
+	eq(null, 'lol');
+} catch (e){
+	console.assert(strip(e.message).includes(`null !== "lol"`));
+}
+
+try {
+	eq('foo', 'lol');
+} catch (e){
+	console.assert(strip(e.message).includes(`"foo" !== "lol"`));
+}
+
+try {
+	eq('lol', 'lol');
+} catch (e){
+	console.assert(false);
+}
 
 try {
 	eq({
@@ -32,7 +57,10 @@ try {
 		}
 	})
 } catch(e) { // todo full assert with chalk
-	console.assert(e.message.includes('foo.bar.qux.lolcat.bip.yup'));
-	console.assert(e.message.includes('56.3'));
-	console.error(e);
+	console.assert(strip(e.message).includes(` ↔ foo.bar.qux.lolcat.bip.yup: 56.3 !== 56
+ ← foo.bar.qux.mlop: Object {}
+ → foo.bar.qux.mlep: Object {
+  "cool": "tes",
+  "flip": 878.964,
+}`));
 }
